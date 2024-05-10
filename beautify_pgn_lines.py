d95e92eeb953fd3afdd6110163e984507283d043
anton <anton.bil@hz.nl>
2024-05-10 14:01:53 +0200

    add virtual keyboard in text-input-screen; refactor

--- beautify_pgn_lines.py
+++ beautify_pgn_lines.py
@@ -119,7 +119,7 @@ class PgnDisplay:
                     number = i
                     numbers.append(number)
                     times = times + 1
-                    if line.startswith(black_search) or black_move_with_white_before in line:  # or white_move in line:
+                    if line.startswith(black_search) or black_move_with_white_before in line or white_move in line:
                         numbers=[i]
                         break
                 i = i + 1
