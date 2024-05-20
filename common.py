eb7408f0b5a18577cc063563f65edd492fd5bfe6
anton <anton.bil@hz.nl>
2024-05-20 10:58:33 +0200

    start of help for pgn-viewer etc

--- common.py
+++ common.py
@@ -35,8 +35,31 @@ using the startmode in the command-line-options.
 
 This part of the help-menu describes the Pgn-viewer and Pgn-editor mode.
 
-PGN-Viewer-mode.
-(A)
+PGN-Editor-mode.
+In this mode you can manually add and edit moves.
+This mode has two sub-modes:
+- PGN Move entry mode
+- Variations-edit mode
+(A)PGN Move entry mode
+In this mode you can enter moves in a sequential way.
+You can add moves using the board, or remove the last move.
+(B) In this mode you can restore the last alternative that you started in Variations-edit mode. 
+(B) Variations-edit mode
+In the mode you can add and modify alternative lines for a particular move.
+(B1) got to particular move using Previous and Next
+-use the top-line of the board to go to a relative position
+- or select the move in the moves-list by clicking it
+(B2) Add an alternative using Alternative and Alternative Manual
+- Alternative: add alternative that the engine suggests
+- Alternative Manual: add move manually, and ask the engine to add the rest of the alternative line
+(B3) Modify alternatives inside moves
+- promote an alternative/variation so it will temporarily act as main line
+If a variation as selected in the way, moves can be added using the PGN Move Entry sub-mode
+Do not forget to Restore the original order of the variations ("Restore alternative" option in both sub-modes)
+- remove an alternative variation
+(B4) Add a Manual variation
+- add the spefific move on the board.
+- the sub-mode is automatically changed to PGN Move entry mode so you can add the rest of the variation-moves manually
 
 """
 
